839406701efa4ef5eec79e30fffdd38d36b93c1b
Edward Schwartz <edwardcschwartz@gmail.com>
2019-06-17 14:56:48 -0400

    working on dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+
 
 
 class App extends Component {
@@ -9,11 +11,15 @@ class App extends Component {
       <div className="app-container">
         <Switch>
           <Route exact path="/" component={LandingPage} />
+          <Route exact path="/nav" component={NavDashboard} />
           <Route exact path="/createuser" component={CreateUser} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/dashboard/profile" component={ProfileSettings} />
           <Route exact path="/dashboard/profile/edit" component={UpdateUser} />
-          <Route exact path="/dashboard/budgetTracker" component={BudgetTracker} />
+          <Route exact path="/dashboard/budget" component={BudgetTracker} />
+          <Route exact path="/dashboard/budget/edit" component={UpdateBudget} />
+          <Route exact path="/dashboard/creditcards" component={CreditCardComponent} />
+          <Route exact path="/dashboard/creditcards/edit" component={UpdateCreditCard} />
         </Switch>
       </div>
     )
---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div className="landing-container">
+                
+
+            </div>
+        )
+    }
+}
+
+
+export default LandingPage
+
+
