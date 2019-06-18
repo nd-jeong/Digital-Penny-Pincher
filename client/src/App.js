5e7cf1dacdfbd246b33ac8a6f1fa951e0410bcc0
Candice Agard <Altovise.Alexis@gmail.com>
2019-06-17 20:12:41 -0400

    animated background seccond layer completed

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,24 @@ import LandingPage from "./components/LandingPage";
 
 
 
-class App extends Component {
-  render() {
+    class App extends Component {
+render() {
     return (
       <div className="app-container">
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/nav" component={NavDashboard} />
-          <Route exact path="/createuser" component={CreateUser} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/dashboard/profile" component={ProfileSettings} />
-          <Route exact path="/dashboard/profile/edit" component={UpdateUser} />
-          <Route exact path="/dashboard/budget" component={BudgetTracker} />
-          <Route exact path="/dashboard/budget/edit" component={UpdateBudget} />
-          <Route exact path="/dashboard/creditcards" component={CreditCardComponent} />
-          <Route exact path="/dashboard/creditcards/edit" component={UpdateCreditCard} />
-        </Switch>
-      </div>
-    )
-  }
-}
-
+         <Route exact path="/" component={LandingPage} />
+{/*{          <Route exact path="/nav" component={NavDashboard} />*!/*/}
+{/*{          <Route exact path="/createuser" component={CreateUser} />*!/*/}
+{/*{          <Route exact path="/dashboard" component={Dashboard} />*!/*/}
+{/*{           <Route exact path="/dashboard/profile" component={ProfileSettings} />*!/*/}
+{/*{           <Route exact path="/dashboard/profile/edit" component={UpdateUser} />*!/*/}
+{/*{           <Route exact path="/dashboard/budget" component={BudgetTracker} />*!/*/}
+{/*{           <Route exact path="/dashboard/budget/edit" component={UpdateBudget} />*!/*/}
+{/*{           <Route exact path="/dashboard/creditcards" component={CreditCardComponent} />*!/*/}
+{/*{           <Route exact path="/dashboard/creditcards/edit" component={UpdateCreditCard} />*!/*/}
+         </Switch>
+       </div>
+      )
+   }
+ }
 export default App;
