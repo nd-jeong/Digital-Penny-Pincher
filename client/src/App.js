d87538c230bf4132c86fb1f3aa76450237292075
Candice Agard <Altovise.Alexis@gmail.com>
2019-06-18 12:00:53 -0400

    logo included in modal

--- client/src/App.js
+++ client/src/App.js
@@ -12,16 +12,16 @@ render() {
       <div className="App-container">
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/nav" component={NavDashboard} />
-          <Route exact path="/createuser" component={CreateUser} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/dashboard/keypad" component={KeypadButtons} />
-          <Route exact path="/dashboard/profile" component={ProfileSettings} />
-          <Route exact path="/dashboard/profile/edit" component={UpdateUser} />
-          <Route exact path="/dashboard/budget" component={BudgetTracker} />
-          <Route exact path="/dashboard/budget/edit" component={UpdateBudget} />
-          <Route exact path="/dashboard/creditcards" component={CreditCardComponent} />
-          <Route exact path="/dashboard/creditcards/edit" component={UpdateCreditCard} />
+          {/*<Route exact path="/nav" component={NavDashboard} />*/}
+          {/*<Route exact path="/createuser" component={CreateUser} />*/}
+          {/*<Route exact path="/dashboard" component={Dashboard} />*/}
+          {/*<Route exact path="/dashboard/keypad" component={KeypadButtons} />*/}
+          {/*<Route exact path="/dashboard/profile" component={ProfileSettings} />*/}
+          {/*<Route exact path="/dashboard/profile/edit" component={UpdateUser} />*/}
+          {/*<Route exact path="/dashboard/budget" component={BudgetTracker} />*/}
+          {/*<Route exact path="/dashboard/budget/edit" component={UpdateBudget} />*/}
+          {/*<Route exact path="/dashboard/creditcards" component={CreditCardComponent} />*/}
+          {/*<Route exact path="/dashboard/creditcards/edit" component={UpdateCreditCard} />*/}
         </Switch>
       </div>
     )
