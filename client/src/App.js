ce05de1881839a5fe36188458514d78a7c9fba3f
Edward Schwartz <edwardcschwartz@gmail.com>
2019-06-18 13:35:59 -0400

    all main components are built and connected. next stop more functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import NavDashboard from "./components/NavDashboard";
 import Dashboard from "./components/Dashboard";
+import ProfileSettings from "./components/ProfileSettings";
+import BudgetTracker from "./components/BudgetTracker";
+import CreditCardComponent from "./components/CreditCardComponent"
 
 
 
@@ -17,13 +20,12 @@ class App extends Component {
           <Route exact path="/nav_dashboard" component={NavDashboard} />
           {/* <Route exact path="/createuser" component={CreateUser} /> */}
           <Route exact path="/dashboard" component={Dashboard} />
-          {/* <Route exact path="/dashboard/keypad" component={KeypadButtons} /> */}
-          {/* <Route exact path="/dashboard/profile" component={ProfileSettings} />
-          <Route exact path="/dashboard/profile/edit" component={UpdateUser} />
+          <Route exact path="/dashboard/profile" component={ProfileSettings} />
+          {/* <Route exact path="/dashboard/profile/edit" component={UpdateUser} /> */}
           <Route exact path="/dashboard/budget" component={BudgetTracker} />
-          <Route exact path="/dashboard/budget/edit" component={UpdateBudget} />
+          {/* <Route exact path="/dashboard/budget/edit" component={UpdateBudget} /> */}
           <Route exact path="/dashboard/creditcards" component={CreditCardComponent} />
-          <Route exact path="/dashboard/creditcards/edit" component={UpdateCreditCard} /> */}
+          {/* <Route exact path="/dashboard/creditcards/edit" component={UpdateCreditCard} /> */}
         </Switch>
       </div>
     )
--- client/src/components/NavDashboard.js
+++ client/src/components/NavDashboard.js
@@ -8,7 +8,7 @@ class NavDashboard extends Component {
         <div>Icon here?</div>
         <Link to={"/dashboard/profile"}> Profile Settings </Link>
         <Link to={"/dashboard/budget"}>My Budget Activity</Link>
-        <Link to={"/dashboard/creditcards"}>Add/Remove Credit Card</Link>
+        <Link to={"/dashboard/creditcards"}>Credit and Debit Cards</Link>
         {/* The "sign out" link redirects to LandingPage (temporary placeholder until we incorporate auth) */}
         <Link to={"/"}>Sign out</Link>
       </div>
