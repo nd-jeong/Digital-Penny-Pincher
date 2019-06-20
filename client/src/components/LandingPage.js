8f94f52e0dc17431c006e7fdc5eba42f1aa78217
Andy Jeong <unjin89@gmail.com>
2019-06-19 23:18:39 -0400

    completed CreateUser and UserSignIn components with complete functionality. Added if statement to balance update function in dashboard.js because new users with no transactions were returning an error

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,34 +4,15 @@ import axios from 'axios';
 
 
 class LandingPage extends Component {
-    constructor() {
-        super();
-
-        this.state= {
-            users: []
-        }
-    }
-
-    async componentDidMount() {
-        const res = await axios.get('http://localhost:4567/users');
-        
-        this.setState({
-            users: res.data
-        });
-    }
-
   render() {
-      const users = this.state.users.map(user => {
-        return <Link to={`/dashboard/${user.id}`}>{user.name}</Link>
-      });
     return (
       <div className="landing-container">
 
           <h2 style={{ padding: "10px 20px", textAlign: "center", color: "lightgreen"}}>Digital Penny Pincher</h2>
-        <Link to="/dashboard"> Dashboard </Link>
-        {users}
+        <Link to='/createuser'>Create User</Link>
+        <Link to='/signin'>Sign In</Link>
 
-  <ul class="bubbles">
+  <ul className="bubbles">
 
           <li></li>
           <li></li>
